0cf8535e51b625068b8352150f8e886959381c48
Louis Orleans <louis@orleans.io>
2020-03-16 03:51:08 -0700

    ✨ respond to request

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@
 import os
 import random
 import functools
-import numpy as np 
+import numpy as np
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from pony.orm import *
 
-#from database import *
+from database import *
 
 load_dotenv()
 TOKEN = str(os.getenv('DISCORD_TOKEN'))
@@ -18,40 +18,41 @@ TOKEN = str(os.getenv('DISCORD_TOKEN'))
 
 client = discord.Client()
 
+
 @db_session
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+  print(f'{client.user.name} has connected to Discord!')
 
-    guilds = client.guilds
-    voice_channels = []
-    virgins = []
+  guilds = client.guilds
+  voice_channels = []
+  virgins = []
 
-    for guild in client.guilds:
-        for channel in guild.channels:
-            print(f'{channel},{channel.type}')
-            if channel.type == discord.ChannelType.voice and channel != guild.afk_channel:
-                virgins.append(channel.members)
-    
-    print(virgins)
+  for guild in client.guilds:
+    for channel in guild.channels:
+      print(f'{channel},{channel.type}')
+      if channel.type == discord.ChannelType.voice and channel != guild.afk_channel:
+        virgins.append(channel.members)
+
+  print(virgins)
+
 
 @db_session
 @client.event
 # /myvirginity
 async def on_message(message):
-    print(message)
-    if message.author == client.user:
-        return
-    if message.content.startswith('/myvirginity'):
-        virgin = select(v for v in Virgin if v.is_user(message.author.name, message.author.discriminator))
-        await message.channel.send(virgin.virginity_score)
+  print(message)
+  if message.author == client.user:
+    return
+  if message.content.startswith('/myvirginity'):
+    await message.channel.send(get_users_virginity(message.author.name, message.author.discriminator).virginity_score)
 
 # @bot.command(name='biggestvirgin')
 # async def biggestvirgin(ctx):
 #     if message.author == client.user:
 #         return
 
-#     response = 
+#     response =
 #     await ctx.send(response)
 
 
@@ -60,9 +61,8 @@ async def on_message(message):
 #     if message.author == client.user:
 #         return
 
-#     response = 
+#     response =
 #     await ctx.send(response)
 
 
 client.run(TOKEN)
-
